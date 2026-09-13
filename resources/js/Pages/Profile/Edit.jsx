24d0bb02cbe189da5370abd6e43bbad734c2d9fd
Milica <milicadj26@gmail.com>
2026-09-13 16:54:12 +0200

    Sredjen Profile page

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -1,6 +1,8 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
-import { useState } from 'react';
+import DeleteUserForm from './Partials/DeleteUserForm';
+import UpdatePasswordForm from './Partials/UpdatePasswordForm';
+import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm';
 
 export default function Edit({auth, mustVerifyEmail, status}){
     return (
@@ -10,10 +12,25 @@ export default function Edit({auth, mustVerifyEmail, status}){
         >
             <Head title="Profile" />
 
-            <div className="py-12">
+            <div className="py-12 flex-1 overflow-y-auto">
+                <div className="mx-auto max-w-7xl space-y-6 sm:px-6 lg:px-8">
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
+                        <UpdateProfileInformationForm
+                            mustVerifyEmail={mustVerifyEmail}
+                            status={status}
+                            className="max-w-xl"
+                        />
+                    </div>
+
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
+                        <UpdatePasswordForm className="max-w-xl" />
+                    </div>
+
+                    <div className="bg-white p-4 shadow sm:rounded-lg sm:p-8 dark:bg-gray-800">
+                        <DeleteUserForm className="max-w-xl" />
+                    </div>
+                </div>
             </div>
         </AuthenticatedLayout>
     );
-
-
 }
